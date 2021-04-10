7ecc495f3af59076d899121883659270b9fe1f66
Floris Meccanici <floris@homedesignshops.nl>
2021-04-10 19:28:51 +0200

    episode 10 finished

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
-Vue.component('message', {
-    props: ['title', 'body'],
-
-    data() {
-        return {
-            isVisible: true
-        };
-    },
-
-
+Vue.component('modal', {
     template: `
-        <article class="message" v-show="isVisible">
-            <div class="message-header">
-                {{ title }}
-                
-                <button type="button" @click="hideModal">x</button>
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+                <div class="box">
+                    <slot></slot>
+                </div>
             </div>
-            <div class="message-body">
-                {{ body }}
-            </div>
-        </article>
-    `,
-
-    methods: {
-        hideModal() {
-            this.isVisible = false;
-        }
-    }
+            <button class="modal-close is-large" @click="$emit('close')"></button>
+        </div>`
 });
 
 new Vue({
-    el: '#root'
+    el: '#root',
+
+    data: {
+        showModal: false
+    }
 });
